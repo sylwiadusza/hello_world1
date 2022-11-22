cf8db69ee732bab1bb3fcd38d3d1b2ea10d52ac0
sylwia <sylwiadusza280@gmail.com>
2022-11-22 20:31:24 +0100

    Dodanie_scanner

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String name="Sylwia";
-        int age=33;
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Podaj swoje imię");
+        String name=scanner.nextLine();
+        System.out.println("Podaj swój wiek");
+        int age=scanner.nextInt();
         System.out.println("Czesc mam na imię "+name+" mam "+age+" lat");
-
     }
 
 }
